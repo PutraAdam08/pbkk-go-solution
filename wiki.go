a7749b67c792f6bf6f95754d1c928f84a38e3357
PutraAdam08 <putraadam322@gmail.com>
2024-11-05 13:07:23 +0700

    add function literals and closure

--- wiki.go
+++ wiki.go
@@ -1,16 +1,17 @@
 package main
+
 import (
-	"fmt"
-	"os"
+	"errors"
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
+	"os"
 	"regexp"
 )
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
@@ -30,7 +31,7 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	//t, err := template.ParseFiles(tmpl + ".html")
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
@@ -42,63 +43,70 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
 	}*/
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request)(string,error){
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
 	if m == nil {
-		http.NotFound(w,r)
+		http.NotFound(w, r)
 		return "", errors.New("invalid Page Title")
 	}
 	return m[2], nil
 }
 
-func makeHandler(fn func (http.ResponseWriter, *http.Request, title string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request)
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	//title := r.URL.Path[len("/view/"):]
-	title, err := getTitle(w, r)
-	if err != nil {
-        return
-    }
+	/*title, err := getTitle(w, r)
+		if err != nil {
+	        return
+	    }*/
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 	}
 	//t, _ := template.ParseFiles("view.html")
 	//t.Execute(w, p)
-	renderTemplate(w , "view", p)
-    //fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	//title := r.URL.Path[len("/edit/"):]
-	title, err := getTitle(w, r)
-	if err != nil {
-        return
-    }
+	/*title, err := getTitle(w, r)
+		if err != nil {
+	        return
+	    }*/
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	/*fmt.Fprint(w, "<h1>Editing %s</h1>" +
-		"<form action=\"/save/%s\" method=\"POST\">"+
-        "<textarea name=\"body\">%s</textarea><br>"+
-        "<input type=\"submit\" value=\"Save\">"+
-        "</form>",
-		p.Title, p.Title, p.Body, 
-	)*/
+			"<form action=\"/save/%s\" method=\"POST\">"+
+	        "<textarea name=\"body\">%s</textarea><br>"+
+	        "<input type=\"submit\" value=\"Save\">"+
+	        "</form>",
+			p.Title, p.Title, p.Body,
+		)*/
 	//t, _ := template.ParseFiles("edit.html")
 	//t.Execute(w, p)
-	renderTemplate(w , "edit", p)
+	renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	//title := r.URL.Path[len("/save/"):]
-	title, err := getTitle(w, r)
-	if err != nil {
-        return
-    }
+	/*title, err := getTitle(w, r)
+		if err != nil {
+	        return
+	    }*/
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -113,9 +121,9 @@ func main() {
 	/*p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	p1.save()
 	p2, _ := loadPage("TestPage")*/
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 	//fmt.Println(string(p2.Body))
 }
