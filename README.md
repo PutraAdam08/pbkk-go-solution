58746c1b70ef5fff3b5ea2f161db3dc05835ce31
PutraAdam08 <putraadam322@gmail.com>
2024-11-11 09:05:57 +0700

    Add gorm

---
+++ README.md
@@ -0,0 +1,14 @@
+# 1. Initial commit
+## go testing
+![alt text](image.png)
+
+# 2. Make a basic post web using go
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
+
+# 3. Make connection with Database
+![alt text](image-4.png)
+
+# 4. Make basic GORM Test
+![alt text](image.png)
---
+++ gorm-trial/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/recordings?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("fail to connect db")
+	}
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: "D11", Price: 25})
+
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "code = ?", "D11")
+	db.Model(&product).Update("Price", 200)
+
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - delete product
+	//db.Delete(&product, 1)
+
+}
